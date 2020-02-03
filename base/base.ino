eaab6f126c82740a44b9fd9d3d492c43aec42ce4
Alec Cox <avlec@w134-87-143-028.wireless.uvic.ca>
2020-02-02 21:22:29 -0800

    Flushed out framework for code

--- base/base.ino
+++ base/base.ino
@@ -1,41 +1,58 @@
 #include "scheduler.h"
 #include "exponentialaveragingfilter.h"
 
+// Analog pin for joystick x
+#define PIN_JOYSTICK_X 9
+// Analog pin for joystick y
+#define PIN_JOYSTICK_Y 8
+// Digital pin for joystick switch
+#define PIN_JOYSTICK_SWITCH 21
+// Analog pin for photoresistor
+#define PIN_PHOTORESISTOR 10
+
 struct message {
   uint8_t x_pos;
   uint8_t y_pos;
   uint8_t pressed;
 };
 
+struct message global_message = { 0, 0, 0 };
 
+CREATE_EXPONENTIAL_FILTER(joystick_x);
+CREATE_EXPONENTIAL_FILTER(joystick_y);
 
 
 void update_remote() {
   // send global message
 }
 
-void check_target() {
-  // check for signal
-
-
-  // if signal, set done flag
-}
-
-void joystick_handler() {
-  // read in joystick data to message
-}
-
-void setup() {
-  // 
-  Scheduler_Init();
-  Scheduler_StartTask(0, 500, update_remote);
-  Scheduler_StartTask(0, 500, check_target);
-  Scheduler_StartTask(0, 500, joystick_handler);
-
+void update_lcd() {
   
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void update_from_external() {
+  // read in joystick data to message
+  // update 
+}
 
+void setup() {
+  Scheduler_Init();
+  Scheduler_StartTask(0, 250, update_remote);
+  Scheduler_StartTask(0, 500, update_lcd);
+  Scheduler_StartTask(0, 100, update_from_external);
+
+  pinMode(PIN_JOYSTICK_X, INPUT);
+  init_exponential_average_filter(&joystick_x, 0.75, PIN_JOYSTICK_X);
+  pinMode(PIN_JOYSTICK_Y, INPUT);
+  init_exponential_average_filter(&joystick_y, 0.75, PIN_JOYSTICK_Y);
+  
+  pinMode(PIN_JOYSTICK_SWITCH, INPUT);
+  pinMode(PIN_PHOTORESISTOR, INPUT);
+}
+
+void loop() {
+  uint32_t idle_time = Scheduler_Dispatch();
+  if (idle_time) {
+    delay(idle_time);
+  }
 }
