44782c8aefaf81d909a7233013a8f09e3640ffe4
Alec Cox <avlec@w134-87-143-028.wireless.uvic.ca>
2020-02-02 21:21:17 -0800

    modified averaging filter to track the arduino analog pin it monitors

--- base/exponentialaveragingfilter.h
+++ base/exponentialaveragingfilter.h
@@ -9,28 +9,27 @@ int DEAD_BAND_MIDDLE_Y = 512;
 struct float_lowpass_filter {
   float alpha;
   float y_prev;
+  unsigned int pin;
 };
-  
-float flpf_getx(int x, struct float_lowpass_filter filter) {
-  if((x > (DEAD_BAND_MIDDLE_X - DEAD_BAND_VARIANCE)) && (x < (DEAD_BAND_MIDDLE_X + DEAD_BAND_VARIANCE)))
-    x = DEAD_BAND_MIDDLE_X;
-  filter.y_prev = filter.alpha*x + (1.0 - filter.alpha)*filter.y_prev;
-  return filter.y_prev;
-}
-
-float flpf_gety(int x, struct float_lowpass_filter filter) {
-  if((x > (DEAD_BAND_MIDDLE_Y - DEAD_BAND_VARIANCE)) && (x < (DEAD_BAND_MIDDLE_Y + DEAD_BAND_VARIANCE)))
-    x = DEAD_BAND_MIDDLE_Y;
-  filter.y_prev = filter.alpha*x + (1.0 - filter.alpha)*filter.y_prev;
-  return filter.y_prev;
-}
 
 struct float_lowpass_filter GLOBAL_X_LOWPASS_FILTER;
 struct float_lowpass_filter GLOBAL_Y_LOWPASS_FILTER;
 
-#define ANALOGINITX(A) GLOBAL_X_LOWPASS_FILTER.alpha = A; GLOBAL_X_LOWPASS_FILTER.y_prev = analogRead(X_PIN)
-#define ANALOGREADX() flpf_getx(analogRead(X_PIN), GLOBAL_X_LOWPASS_FILTER)
-#define ANALOGINITY(A) GLOBAL_Y_LOWPASS_FILTER.alpha = A; GLOBAL_Y_LOWPASS_FILTER.y_prev = analogRead(Y_PIN)
-#define ANALOGREADY() flpf_gety(analogRead(Y_PIN), GLOBAL_Y_LOWPASS_FILTER)
+#define CREATE_EXPONENTIAL_FILTER(NAME) struct float_lowpass_filter NAME
+
+void init_exponential_average_filter(struct float_lowpass_filter* filter, float alpha, int pin) {
+  filter->alpha = alpha;
+  filter->y_prev = analogRead(pin);
+  filter->pin = pin;
+}
+
+float run_exponential_average_filter(struct float_lowpass_filter* filter) {
+  int val = analogRead(filter->pin);
+  if((val > (DEAD_BAND_MIDDLE_X - DEAD_BAND_VARIANCE)) && (val < (DEAD_BAND_MIDDLE_X + DEAD_BAND_VARIANCE)))
+    val = DEAD_BAND_MIDDLE_X;
+  filter->y_prev = filter->alpha*val + (1.0 - filter->alpha)*filter->y_prev;
+  return filter->y_prev;
+}
+
 
 #endif //EXPONENTIALAVERAGINGFILTER_H
